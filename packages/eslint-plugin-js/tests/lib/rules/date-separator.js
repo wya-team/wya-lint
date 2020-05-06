93f374acd43ba8778f941be2a8222ca2190276b3
蒋东 <852778359@qq.com>
2020-05-06 14:32:10 +0800

    fix: 格式转换

--- packages/eslint-plugin-js/tests/lib/rules/date-separator.js
+++ packages/eslint-plugin-js/tests/lib/rules/date-separator.js
@@ -2,9 +2,9 @@ const rule = require("../../../lib/rules/date-separator"); // 引入rule
 const RuleTester = require("eslint").RuleTester;
 
 const ruleTester = new RuleTester({
-    parserOptions: {
-        ecmaVersion: 7, // 默认支持语法为es5 
-    },
+	parserOptions: {
+		ecmaVersion: 7, // 默认支持语法为es5 
+	},
 });
 
 ruleTester.run('date-separator', rule, {
@@ -20,9 +20,9 @@ ruleTester.run('date-separator', rule, {
 			code: 'const date = new Date("2020-01-01")',
 			output: 'const date = new Date("2020/01/01")',
 			errors: [{
-                message: '字符串日期禁止使用"-"作为分隔符，在IOS内会报错', // 与rule抛出的错误保持一致
-                type: 'NewExpression' // rule监听的对应钩子
-            }]
+				message: '字符串日期禁止使用"-"作为分隔符，在IOS内会报错', // 与rule抛出的错误保持一致
+				type: 'NewExpression' // rule监听的对应钩子
+			}]
 		}
 	]
 })
